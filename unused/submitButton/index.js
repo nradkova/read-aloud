51d56b6d825cd89a7627d2856a51d3feacb7d118
nradkova <neliradkova@abv.bg>
2021-12-22 18:42:52 +0200

    unused files removed

--- unused/submitButton/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css'
-const SubmitButton = ({ title, onSubmit }) => {
-    return (
-      <button type="submit" onSubmit={onSubmit}>{title}</button>
-    )
-  }
-
-  export default SubmitButton
